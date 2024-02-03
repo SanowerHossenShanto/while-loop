7fce876ebbfe39cbcb929d1af083e02c34c43bbf
SanowerHossenShanto <ssarker515@gmail.com>
2024-02-04 01:42:02 +0600

    added

---
+++ evensum.js
@@ -0,0 +1,17 @@
+let num=1;
+let sum=0;
+while(num<=10)
+{
+    if(num%2==0){
+        console.log("Even number is:", num);
+        // sum=sum+num;
+        // console.log("Sum of even number is:",sum);
+    }
+    else{
+        console.log("Odd number is:", num);
+        // sum=sum+num;
+        // console.log("Sum of odd number is:",sum);
+    }
+    num++;
+
+}
